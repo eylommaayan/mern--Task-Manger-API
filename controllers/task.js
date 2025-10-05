1e95e25936e35521ef7062692fc80e6a5a38f1cd
eylommaayan <eylonmaayan@gmail.com>
2025-10-05 09:31:53 +0300

    delete taks

--- controllers/task.js
+++ controllers/task.js
@@ -37,13 +37,14 @@ const updateTask = async (req, res) => {
 const deleteTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
-    const task = await Task.findByIdAndDelete(taskID); // ← מחיקה בפועל
+    const task = await Task.findOneAndDelete({ _id: taskID });
     if (!task) {
       return res.status(404).json({ msg: `No task with id: ${taskID}` });
     }
-    return res.status(204).send(); // אין גוף תגובה (אפשר גם 200 עם {msg:'deleted'})
-  } catch (error) {
-    return res.status(500).json({ msg: error.message });
+   res.status(200).json({task });
+  }
+  catch (error) {
+    res.status(500).json({ msg: error.message })
   }
 };
 
